4ee8e56223a7317b27d86cb549259044f8cdf9f5
kaylajrox <krodriguez106@att.net>
2016-03-19 17:32:34 -0700

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- Homework/Assignment_2/Gaddis_8thEd_Chap9_Prob1/main.cpp
+++ Homework/Assignment_2/Gaddis_8thEd_Chap9_Prob1/main.cpp
@@ -60,7 +60,7 @@ int *inputI(int &size){
 }
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
-//                  Display Integer Array
+//                           Display Integer Array
 void display(int *array,int &size){
     for(int i=0;i<size;i++){
         cout<<*(array+i)<<" ";
