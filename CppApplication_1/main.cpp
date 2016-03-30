6494db7eeb91e7231d7673408de4e010e69efb72
kaylajrox <krodroguez106@att.net>
2016-03-30 15:00:49 -0700

    Signed-off-by: kaylajrox <krodroguez106@att.net>

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: Kayla Rodriguez
+ * Title: Average Number of Letters
+ * Problem: Gaddis_8thEd_Chap10_Prob4
+ *
+    Problem 3:
+  Write a function that accepts a pointer to a C-string as an argument and
+  returns the number of words contained in the string. For instance, if the
+  string argument is “Four score and seven years ago” the function should return
+  the number 6. Demonstrate the function in a program that asks the user to
+  input a string and then passes it to the function. The number of words in the
+  string should be displayed on the screen. Optional Exercise: Write an
+  overloaded version of this function that accepts a string class object
+   as its argument.
+    Problem 4:
+  Modify the program you wrote for Problem 3 (Word Counter), so it also
+  displays the average number of letters in each word.
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv){
+    //Declare Variables   
+    char str[50];
+    int count=0;
+    //Input
+    cout<<"Please input a string (any word or sentence) and press enter "<<endl;
+    cin.getline(str,50);
+   
+    for (int i=0;i<strlen(str);i++){
+        if (str[i]==' '){
+            count++;
+        }
+    }
+    if(strlen(str)>0)
+        count++;
+    cout<<"There were "<<count<<" words."<<endl;
+    return 0;
+}
