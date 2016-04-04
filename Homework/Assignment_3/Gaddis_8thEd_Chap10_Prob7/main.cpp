1db6b1bafe22715dc9949fa8ffd8e1f04aee4665
kaylajrox <krodriguez106@att.net>
2016-04-04 15:53:44 -0700

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- Homework/Assignment_3/Gaddis_8thEd_Chap10_Prob7/main.cpp
+++ Homework/Assignment_3/Gaddis_8thEd_Chap10_Prob7/main.cpp
@@ -2,11 +2,11 @@
  Title: Sentence Filter
  Problem:
   Write a program that asks the user for two file names.
-  The first file will be opened for input and the second file will be opened
-  for output. (It will be assumed that the first file contains sentences that
-  end with a period.) The program will read the contents of the first file and
-  change all the letters to lowercase except the first letter of each sentence,
-  which should be made uppercase. The revised contents should be stored in the
+  The first file will be opened for input and the second file will be opened 
+  for output. (It will be assumed that the first file contains sentences that 
+  end with a period.) The program will read the contents of the first file and 
+  change all the letters to lowercase except the first letter of each sentence, 
+  which should be made uppercase. The revised contents should be stored in the 
   second file.       */
 //System Libraries
 #include <bits/stdc++.h>
@@ -21,7 +21,7 @@ using namespace std;
 //Function Prototypes
 //void rdFile(ifstream&   infile);
 //Execution Begins Here
-int main(int argc, char** argv){
+int main(int argc, char** argv){    
     //Open the File
     ifstream infile;
     infile.open("input.txt");
@@ -42,18 +42,18 @@ int main(int argc, char** argv){
                 //reset flag
                 isfirst=false;
             }
-            else
+            else 
                 //write all other characters after period that are not a letter
                 outfile.put(ch);
-
+            
         }
         else
             //write all other characters in lower case
-          outfile.put(tolower(ch));
-
+          outfile.put(tolower(ch)); 
+        
         if (ch=='.')
             isfirst=true; //this is the end of sentence so reads next character
-
+        
         //Reading the next character
         ch=infile.get();
     }
@@ -61,8 +61,8 @@ int main(int argc, char** argv){
     outfile.close();
     //Read the File
 //    rdFile();
-
-
+    
+   
     return 0;
 }
 ////000000001111111112222222222333333333344444444445555555555666666666677777777778
@@ -74,11 +74,11 @@ int main(int argc, char** argv){
 //        infile>>names[i];
 //    }
 //}
-////    //Declare variables
+////    //Declare variables 
 ////    ifstream file1;
 ////    ofstream file2;
 ////    file2;
-////    //Input
+////    //Input 
 ////    cout<<"Input two file names. The first will be opened for input and the ";
 ////    cout<<"second will be opened for output "<<endl;
 ////    cout<<"Enter the first file name and press enter."<<endl;
