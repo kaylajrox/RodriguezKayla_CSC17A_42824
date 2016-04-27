692b3996775be82f350c8981121036d2157c8c55
kaylajrox <krodriguez106@att.net>
2016-04-27 13:39:30 -0700

    Signed-off-by: kaylajrox <krodriguez106@att.net>

---
+++ Class/Craps_V1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on April 27, 2016, 12:54 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
