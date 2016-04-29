a5f7feadff749bbdee172da23a5507fa9f3e34c6
kaylajrox <krodriguez106@att.net>
2016-04-28 21:30:39 -0700

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- Homework/Assignment_4/Gaddis_8thEd_Chap14_Prob1UNF/Numbers.cpp
+++ Homework/Assignment_4/Gaddis_8thEd_Chap14_Prob1UNF/Numbers.cpp
@@ -5,11 +5,21 @@
  * Created on April 27, 2016, 10:49 PM
  */
 
-#include "Numbers.h"
+//system libraries
 #include <string>
+#include <iostream>
 using namespace std;
+//user libraries
+#include "Numbers.h"
+//do we still have to initialize even if we have set a value to it?
   Numbers::Numbers() {
-    number = 0;
+    number = 0;  
+//    hundred="";
+//    thousand="";
+//    lessThan20[SIZE] = {"", "","","","","",
+//    "","","","","","","","","",
+//    "","","","", ""}; 
+//    SIZE=0;
 }
 void Numbers::print(int amount){
     number=amount;
@@ -25,30 +35,22 @@ void Numbers::print(int amount){
     string result="";
     
     switch(n1000s){
-        case 9:  result+="Nine Thousand";break; 
-        case 8:  result+="Eight Thousand ";break;
-        case 7:  result+="Seven Thousand ";break;
-        case 6:  result+="Six Thousand ";break;
-        case 5:  result+="Five Thousand ";break;
-        case 4:  result+="Four Thousand ";break;
-        case 3:  result+="Three Thousand ";break;
-        case 2:  result+="Two Thousand ";break;
-        case 1:  result+="One Thousand ";break;
+        for(int i=1;i<=10;i++){
+        lessThan20[i]; 
+        if (i=n1000s)
+            result+=lessThan20[i]; 
+        result+=thousand;
+        }
     }
     //Output the 100's
     switch(n100s){
-        case 9:  result+="Nine Hundred ";break; 
-        case 8:  result+="Eight Hundred ";break;
-        case 7:  result+="Seven Hundred ";break;
-        case 6:  result+="Six Hundred ";break;
-        case 5:  result+="Five Hundred ";break;
-        case 4:  result+="Four Hundred ";break;
-        case 3:  result+="Three Hundred ";break;
-        case 2:  result+="Two Hundred ";break;
-        case 1:  result+="One Hundred ";break;
+        for(int i=1;i<=10;i++){
+        lessThan20[i]; 
+        if (i=n100s)
+            result+=lessThan20[i]; 
+        result+=hundred;
+        }
     }
-
-    //Output the 10's
     switch(n10s){
         case 9:  result+="Ninety ";break;
         case 8:  result+="Eighty ";break;
@@ -58,32 +60,28 @@ void Numbers::print(int amount){
         case 4:  result+="Forty ";break;
         case 3:  result+="Thirty ";break;
         case 2:  result+="Twenty ";break;
-        case 1:  {if(n10s==1&&n1s==0) result+="Ten ";
-        if(n10s==1&&n1s==1) result+="Eleven ";
-        if(n10s==1&&n1s==2) result+="Twelve ";
-        if(n10s==1&&n1s==3) result+="Thirteen ";
-        if(n10s==1&&n1s==4) result+="Fourteen";
-        if(n10s==1&&n1s==5) result+="Fifteen ";
-        if(n10s==1&&n1s==6) result+="Sixteen ";
-        if(n10s==1&&n1s==7) result+="Seventeen ";
-        if(n10s==1&&n1s==8) result+="Eighteen ";
-        if(n10s==1&&n1s==9) result+="Nineteen ";
-        break;}
-    }
-
-    //Output the 1's
-    switch(n1s){
-        if(n10s==0){
-        case 9:  result+="Nine ";break;
-        case 8:  result+="Eight ";break;
-        case 7:  result+="Seven ";break;
-        case 6:  result+="Six ";break;
-        case 5:  result+="Five ";break;
-        case 4:  result+="Four ";break;
-        case 3:  result+="Three ";break;
-        case 2:  result+="Two ";break;
-        case 1:  result+="One ";break;
+        case 1: {
+            for(int i=10;i<=SIZE;i++){
+                lessThan20[i]; 
+                if (i=n1s)
+                    result+=lessThan20[i]; 
+            }
         }
+        break;}
+   
+    if(n10s==0){
+        //Output the 1's
+        switch(n1s){
+            case 9:  result+="Nine ";break;
+            case 8:  result+="Eight ";break;
+            case 7:  result+="Seven ";break;
+            case 6:  result+="Six ";break;
+            case 5:  result+="Five ";break;
+            case 4:  result+="Four ";break;
+            case 3:  result+="Three ";break;
+            case 2:  result+="Two ";break;
+            case 1:  result+="One ";break;
+            }
     }
+    
 }
-
--- Homework/Assignment_4/Gaddis_8thEd_Chap14_Prob1UNF/Numbers.h
+++ Homework/Assignment_4/Gaddis_8thEd_Chap14_Prob1UNF/Numbers.h
@@ -1,7 +1,6 @@
 /* 
  * File:   Numbers.h
  * Author: Kayla
- *
  * Created on April 27, 2016, 10:49 PM
  */
 
@@ -12,10 +11,19 @@ using namespace std;
 class Numbers {
 private:
     int number;
+    static const int SIZE;
+    static string lessThan20[SIZE];
+    static string hundred; 
+    static string thousand; 
 public:
     Numbers();
     void print(int);
 };
-
+const int Numbers::SIZE=20;
+string Numbers::thousand= "thousand";
+string Numbers::thousand= "hundred";
+string Numbers::lessThan20[SIZE]= {"zero", "one","two","three","four","five",
+    "six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen",
+    "fifteen","sixteen","seventeen","eighteen", "nineteen"}; 
 #endif /* NUMBERS_H */
 
