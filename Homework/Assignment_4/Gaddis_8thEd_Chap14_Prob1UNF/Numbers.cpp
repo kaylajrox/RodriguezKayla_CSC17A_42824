a3d040bae1ec32eb904c048d207a4a86dfa7b0ef
kaylajrox <krodriguez106@att.net>
2016-05-09 15:28:41 -0700

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- Homework/Assignment_4/Gaddis_8thEd_Chap14_Prob1UNF/Numbers.cpp
+++ Homework/Assignment_4/Gaddis_8thEd_Chap14_Prob1UNF/Numbers.cpp
@@ -17,8 +17,8 @@ using namespace std;
     number = 0;  
 }
   //initialize static members
-  string Numbers::thousand= "thousand";
-  string Numbers::hundred= "hundred";
+  string Numbers::thousand= "Thousand";
+  string Numbers::hundred= "Hundred";
   string Numbers::lessThan20[SIZE]= {"zero", "one","two","three","four","five",
     "six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen",
     "fifteen","sixteen","seventeen","eighteen", "nineteen"}; 
@@ -37,19 +37,37 @@ using namespace std;
     string result="";
     
     
-    for(int i=0;i<SIZE;i++){
-        result+=lessThan20[i];
-    }
-    result+=" "+thousand;
-        
-    //Output the 100's
-    for(int i=1;i<SIZE;i++){
-        if(i==n100s)
-            result+=lessThan20[i];
+   //Output the 1000's
+    switch(n1000s)
+    {
+        case 9:  result+="Nine "+thousand+" ";break;
+        case 8:  result+="Eight "+thousand+" ";break;
+        case 7:  result+="Seven "+thousand+" ";break;
+        case 6:  result+="Six "+thousand+" ";break;
+        case 5:  result+="Five "+thousand+" ";break;
+        case 4:  result+="Four "+thousand+" ";break;
+        case 3:  result+="Three "+thousand+" ";break;
+        case 2:  result+="Two "+thousand+" ";break;
+        case 1:  result+="One "+thousand+" ";break;
      }
-    result+=" "+hundred;
-//    cout<<result;
-    switch(n10s){
+    
+    //Output the 100's
+    switch(n100s)
+    {
+        case 9:  result+="Nine "+hundred+" ";break;
+        case 8:  result+="Eight "+hundred+" ";break;
+        case 7:  result+="Seven "+hundred+" ";break;
+        case 6:  result+="Six "+hundred+" ";break;
+        case 5:  result+="Five "+hundred+" ";break;
+        case 4:  result+="Four "+hundred+" ";break;
+        case 3:  result+="Three "+hundred+" ";break;
+        case 2:  result+="Two "+hundred+" ";break;
+        case 1:  result+="One "+hundred+" ";break;
+    }
+
+    //Output the 10's
+    switch(n10s)
+    {
         case 9:  result+="Ninety ";break;
         case 8:  result+="Eighty ";break;
         case 7:  result+="Seventy ";break;
@@ -57,15 +75,23 @@ using namespace std;
         case 5:  result+="Fifty ";break;
         case 4:  result+="Forty ";break;
         case 3:  result+="Thirty ";break;
-        case 2:  result+="Twenty ";break;
-        case 1: {
-                result+=lessThan20[number]; 
-        break;}
-   
-    if(n10s==0){
-        //Output the 1's
-        result+=lessThan20[number];         
+        case 2:  result+="Twenty ";break;          
+        case 1:  result+="";break;
+        case 0:  result+="";break;
     }
-cout << result << endl;
- }
+    if(n10s==1){
+        for(int j=1;j<9;j++){
+            if(n1s==j)
+                result+=lessThan20[j+10];
+        }
+    }
+    cout<<result;
+    if(n10s!=1)
+    {
+        for(int i=0;i<10;i++)
+        {
+            if(n1s==i)
+               cout << lessThan20[i];
+        }   
+    }
  }
