0fdfff5e2749e578489680951efc9171651d4109
Kayla Rodriguez <Kayla Rodriguez>
2016-05-20 17:07:00 -0700

    Signed-off-by: Kayla Rodriguez <Kayla Rodriguez>

---
+++ Homework/Assignment_4/Gaddis_8thEd_Chap14_Prob5/TimeOff.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TimeOff.h
+ * Author: Kayla
+ *
+ * Created on May 19, 2016, 5:53 PM
+ */
+
+#ifndef TIMEOFF_H
+#define TIMEOFF_H
+
+class TimeOff {
+public:
+    TimeOff();
+    TimeOff(const TimeOff& orig);
+    virtual ~TimeOff();
+private:
+
+};
+
+#endif /* TIMEOFF_H */
+
