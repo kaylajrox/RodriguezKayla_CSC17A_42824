14dd44bbfbf2694618cf8eadde67b6803dc47a55
Kayla Rodriguez <Kayla Rodriguez>
2016-05-30 22:16:38 -0700

    Signed-off-by: Kayla Rodriguez <Kayla Rodriguez>

---
+++ Homework/Assignment_5/Gaddis_8thEd_Chap16_Prob1/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Date Exceptions  
+ * Modify the  Date  class you wrote for Programming Challenge 1 of Chapter 13. 
+ * The class should implement the following exception classes:   
+ * InvalidDay   
+ * -Throw when an invalid day (< 1 or > 31) is passed to the class.   
+ * InvalidMonth  
+ * - Throw when an invalid month (< 1 or > 12) is passed to the class.  
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Kayla
+ *
+ * Created on May 30, 2016, 10:14 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
