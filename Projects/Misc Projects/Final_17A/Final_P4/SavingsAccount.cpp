37aa445862cb1bc994714136255665374cfdb251
Kayla Rodriguez <Kayla Rodriguez>
2016-06-11 06:19:07 -0700

    Signed-off-by: Kayla Rodriguez <Kayla Rodriguez>

--- Projects/Misc Projects/Final_17A/Final_P4/SavingsAccount.cpp
+++ Projects/Misc Projects/Final_17A/Final_P4/SavingsAccount.cpp
@@ -8,30 +8,36 @@
 using namespace std;
 
 SavingsAccount::SavingsAccount(float x){
-    if(x>0){
-        Balance=x;
-       
+    if(x<0){
+       cout<<"WithDraw not Allowed\n";
+       Balance=0;
     }else{
-        cout<<"WithDraw not Allowed\n";
-        Balance=0;
+        Balance=x;
     }
-    int   FreqWithDraw=0;                 
-    int   FreqDeposit=0;
+    FreqWithDraw=0;                 
+    FreqDeposit=0;
 }             
 void  SavingsAccount::Transaction(float a){
     if(a>0){
-        Balance+=a;
-        FreqDeposit++;
-    }else if (a<0){
-        if(a<(-Balance)){
-            cout<"WithDraw not Allowed\n";
+        Balance=a+Balance;
+        Deposit(Balance);
+    }else if (Balance+a>0){
+        if(a<Balance){
+            Balance+=Withdraw(Balance);
         }else{
-            Balance+=a;
-            FreqWithDraw++;
+            cout<<"WithDraw not allowed\n";
         }
     }
         
-}        
+}    
+float SavingsAccount::Withdraw(float a){
+  FreqWithDraw++;
+  return a;
+}
+float SavingsAccount::Deposit(float a){
+  FreqDeposit++;
+  return a;
+}
 float SavingsAccount::Total(float savint,int time){
     float ttl=Balance;
     for(int i=0;i<time;i++){
@@ -40,7 +46,8 @@ float SavingsAccount::Total(float savint,int time){
     return ttl;
 }	 
 float SavingsAccount::TotalRecursive(float savint,int time){
-    float total=Balance;
+    float total;
+    Balance=total;
     float interest=0.0;
     for(int i=0;i<time;i++){
         interest=total*savint;
